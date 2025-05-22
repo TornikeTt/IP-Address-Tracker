71e81d59b7a6bd60e0afb3ecd7a61829f7aa8923
Tornike <chargeishvilitornike@gmail.com>
2025-05-22 11:09:55 +0400

    add marker icone

--- src/02-Map/Map.jsx
+++ src/02-Map/Map.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
 import { useEffect, useRef } from "react";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from 'leaflet';
-import "./Map.scss"
+import L from "leaflet";
 
-function MapUpdater({lat,lng}) {
+import iconUrl from "../assets/images/icon-location.svg";
+import "./Map.scss";
+
+const customIcon = new L.Icon({
+    iconUrl,
+    iconSize: [32, 32],
+    iconAnchor: [16, 32],
+});
+
+function MapUpdater({ lat, lng }) {
     const map = useMap();
 
     useEffect(() => {
@@ -15,30 +23,22 @@ function MapUpdater({lat,lng}) {
                 duration: 1.5,
             });
         }
-    }, [lat,lng]);
+    }, [lat, lng]);
 
     return null;
 }
 
-function Map({lat, lng}) { 
-    return ( 
-        <MapContainer 
-            className="map" 
-            center={[lat, lng]} 
-            zoom={13}
-        >
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-
-          <Marker position={[lat, lng]} />
-          <MapUpdater lat={lat} lng={lng}/>
+function Map({ lat, lng }) {
+    return (
+        <MapContainer className="map" center={[lat, lng]} zoom={13}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[lat, lng]} icon={customIcon} />
+            <MapUpdater lat={lat} lng={lng} />
         </MapContainer>
-    )
+    );
 }
 
-
 export default Map;
-
-
